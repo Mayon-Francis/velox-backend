ac964c44233b4c9ada026861805227ade433cbbd
Mayon-Francis <mayonfrancis@gmail.com>
2023-09-16 17:30:46 -0400

    login

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const pingRouter = require("./routes/pingRouter");
 const supabaseClient = require("./utils/db");
+const pingRouter = require("./routes/pingRouter");
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 // const MONGO_URI = process.env.MONGO_URI;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 async function main() {
   try {
@@ -15,16 +20,8 @@ async function main() {
       return res.status(200).json({ message: "Hello world!" });
     });
 
-    app.use("/api", pingRouter);
-
-    app.use(cors());
-    app.use(bodyParser.json());
-
-    const { data, error } = await supabaseClient
-      .from('users')
-      .select()
-
-    console.log(data, error)
+    app.use("/ping", pingRouter);
+    app.use("/user", userRouter);
 
     app.listen(PORT, () => {
       console.log("Server is running on port 3000");
---
+++ routes/userRouter.js
@@ -0,0 +1,30 @@
+const { Router } = require("express");
+const supabaseClient = require("../utils/db");
+
+const userRouter = Router();
+
+userRouter.post("/login", async (req, res) => {
+    console.log(req.body);
+    const { data, error } = await supabaseClient
+        .from('users')
+        .select()
+        .eq('email', req.body.email);
+
+    if (data.length === 0) {
+        await supabaseClient
+            .from('users')
+            .insert([
+                req.body
+            ]);
+    } else if (data[0].role !== req.body.role) {
+        return res.status(400).json({ message: "Wrong Role" });
+    }
+
+    return res.status(200).json({
+        message: "Login Success",
+        status: true
+    });
+}
+);
+
+module.exports = userRouter;
