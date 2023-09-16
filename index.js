48f6af5f233e27416ac0b5f65a5826a6fde16dd8
Mayon-Francis <mayonfrancis@gmail.com>
2023-09-16 16:38:03 -0400

    DB

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const pingRouter = require("./routes/pingRouter");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
-});
+async function main() {
+  try {
+    app.get("/", (req, res) => {
+      return res.status(200).json({ message: "Hello world!" });
+    });
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello world!" });
-});
+    app.use("/api", pingRouter);
 
-app.use("/api", pingRouter);
+    app.use(cors());
+    app.use(bodyParser.json());
 
-app.use(cors());
-app.use(bodyParser.json());
+    mongoose.connect(MONGO_URI);
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log("Server is running on port 3000");
+    });
+
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
+
