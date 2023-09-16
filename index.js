14e8fbd3a2cd61952041a63399446332d0b2c614
Jaison Dennis <jaisondennis080@gmail.com>
2023-09-16 16:31:00 -0400

    Updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
