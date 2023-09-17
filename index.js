9beb319fcdedb59ef09e52a2197e7a1bcb102fa9
Jaison Dennis <jaisondennis080@gmail.com>
2023-09-16 20:23:29 -0400

    Task Creation and Accepting Completed

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function main() {
     app.use("/task", taskRouter);
 
     server.listen(PORT);
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port ${PORT}`);
 
 
   } catch (error) {
