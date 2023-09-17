2f97c252486eea27719aec4cc234d419595abb83
Jaison Dennis <jaisondennis080@gmail.com>
2023-09-17 09:18:03 -0400

    Updated

---
+++ routes/statisticsRouter.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const supabaseClient = require("../utils/db");
+
+const statisticsRouter = Router();
+
+statisticsRouter.get("/", async (req, res) => {
+  let totalTasks = await supabaseClient
+    .from("tasks")
+    .select("*");
+
+  let totalTasksCount = totalTasks.data.length;
+
+  let percentCompletedTasks = await supabaseClient
+    .from("tasks")
+    .select("*")
+    .eq("status", "completed");
+
+  let percentage = Math.round((percentCompletedTasks.data.length / totalTasksCount) * 100)
+
+  let totalEmployees = await supabaseClient.from("users").select("*");
+
+  let totalEmployeesCount = totalEmployees.data.length;
+
+  let totalUrgetTasks = await supabaseClient
+    .from("tasks")
+    .select("*")
+    .eq("priority", "high");
+
+  let totalUrgetTasksCount = totalUrgetTasks.data.length;
+
+  return res.status(200).json({
+    message: "Statistics Fetched Successfully",
+    totalTasksCount: totalTasksCount,
+    percentCompletedTasks: percentage,
+    totalEmployeesCount: totalEmployeesCount,
+    totalUrgetTasksCount: totalUrgetTasksCount,
+    status: true,
+  });
+});
+
+module.exports = statisticsRouter;
