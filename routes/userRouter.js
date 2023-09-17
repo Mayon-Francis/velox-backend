4bb738fb30bd4661a57b9e0c85a5e4b868ec489a
Mayon-Francis <mayonfrancis@gmail.com>
2023-09-16 20:43:10 -0400

    get id login fix

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,26 +5,37 @@ const userRouter = Router();
 
 userRouter.post("/login", async (req, res) => {
     console.log(req.body);
-    const { data, error } = await supabaseClient
+    let { data, error } = await supabaseClient
         .from('users')
         .select()
         .eq('email', req.body.email);
 
     if (data.length === 0) {
-        await supabaseClient
+        const { data, error } = await supabaseClient
             .from('users')
             .insert([
                 req.body
-            ]);
+            ])
+            .select("id");
+
+        if (error) {
+            return res.status(500).json({
+                message: "Something went wrong",
+                error: error,
+                status: false,
+            });
+        }
+
+        return res.status(200).json({
+            message: "Login Success",
+            id: data[0].id,
+            status: true
+        });
     } else if (data[0].role !== req.body.role) {
         return res.status(400).json({ message: "Wrong Role" });
     }
 
-    return res.status(200).json({
-        message: "Login Success",
-        id: data[0].id,
-        status: true
-    });
+
 }
 );
 
